0efc3e51e70d69521b8c215cda0edc67f42101d6
CaioLuppo <luppocode@outlook.com>
2022-07-27 20:07:40 -0300

    Adicionando future na lista

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/database/app_database.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bytebank/screens/contact_form.dart';
@@ -5,22 +6,59 @@ import '../models/contact.dart';
 
 class ContactsList extends StatelessWidget {
 
-  final List<Contact> contacts = [];
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Contacts"),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, indx) {
-          final Contact contact = contacts[indx];
-          return _ContactItem(contact);
+      body: FutureBuilder<List<Contact>>(
+        future: findAll(),
+        builder: (context, snapshot){
+
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+              break;
+
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text("Loading")
+                  ],
+                ),
+              );
+
+            case ConnectionState.active:
+              break;
+
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data!;
+
+              if (contacts.isNotEmpty) { // se tiver algum contato
+                return ListView.builder(
+                  itemCount: contacts.length,
+                  itemBuilder: (context, indx) {
+                    final Contact contact = contacts[indx];
+                    return _ContactItem(contact);
+                  },
+                );
+              } else { // caso não tenha encontrado contato
+                return const Center(child: Text("No contacts found."));
+              }
+          }
+
+          return const Center(child: Text("Unknown error."));
+
+          
         },
-        itemCount: contacts.length,
       ),
+
+
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
@@ -37,7 +75,7 @@ class _ContactItem extends StatelessWidget {
 
   final Contact contact;
 
-  _ContactItem( this.contact );
+  const _ContactItem( this.contact );
 
   @override
   Widget build(BuildContext context) {
