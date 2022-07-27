4ed484357e68efc6cf858892c91c9822c9212215
CaioLuppo <luppocode@outlook.com>
2022-07-27 18:38:24 -0300

    Adicionando lista dinâmica

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,28 +1,25 @@
-import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
+import 'package:bytebank/screens/contact_form.dart';
+import '../models/contact.dart';
+
 class ContactsList extends StatelessWidget {
+
+  final List<Contact> contacts = [];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Contacts"),
       ),
-      body: ListView(
-        children: const [
-          Card(
-            child: ListTile(
-              title: Text(
-                "Caio",
-                style: TextStyle(fontSize: 24),
-              ),
-              subtitle: Text(
-                "0000",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, indx) {
+          final Contact contact = contacts[indx];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -31,6 +28,29 @@ class ContactsList extends StatelessWidget {
           );
         },
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+
+  final Contact contact;
+
+  _ContactItem( this.contact );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: const TextStyle(fontSize: 24),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: const TextStyle(fontSize: 16),
+        ),
       ),
     );
   }
