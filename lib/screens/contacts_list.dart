cf7fa2a73ae332585924fb70eac6aad835307a3b
CaioLuppo <luppocode@outlook.com>
2022-08-11 18:52:17 -0300

    Adicionando formulário

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,5 +1,6 @@
 import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
 
 import '../components/loading.dart';
@@ -40,7 +41,9 @@ class _ContactsListState extends State<ContactsList> {
                   itemCount: contacts.length,
                   itemBuilder: (context, indx) {
                     final Contact contact = contacts[indx];
-                    return _ContactItem(contact);
+                    return _ContactItem(contact, onClick: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => TransactionForm(contact),),);
+                    },);
                   },
                 );
               } else { // caso não tenha encontrado contato
@@ -65,13 +68,15 @@ class _ContactsListState extends State<ContactsList> {
 class _ContactItem extends StatelessWidget {
 
   final Contact contact;
+  final Function onClick;
 
-  const _ContactItem( this.contact );
+  const _ContactItem( this.contact, {required this.onClick});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () => onClick(),
         title: Text(
           contact.name,
           style: const TextStyle(fontSize: 24),
