4fb90cf73a1f00378364af66f54d7ae4724e7377
CaioLuppo <luppocode@outlook.com>
2022-08-11 16:46:02 -0300

    Importando widget do curso

---
+++ lib/components/centered_message.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CenteredMessage extends StatelessWidget {
+  final String message;
+  final IconData? icon;
+  final double iconSize;
+  final double fontSize;
+
+  CenteredMessage({
+    required this.message,
+    this.icon,
+    this.iconSize = 64,
+    this.fontSize = 24,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Visibility(
+            visible: icon != null,
+            child: Icon(
+              icon,
+              size: iconSize,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 24.0),
+            child: Text(
+              message,
+              style: TextStyle(fontSize: fontSize),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bytebank/web/webclient.dart';
 import 'package:flutter/material.dart';
 
 // screens
